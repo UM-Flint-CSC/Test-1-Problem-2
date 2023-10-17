3bd59379074db8da523edcf5b25709866c7a2b42
brmcbrid <69523108+brmcbrid@users.noreply.github.com>
2023-10-17 18:49:47 -0400

    Update P2.cpp

--- P2.cpp
+++ P2.cpp
@@ -15,11 +15,6 @@
 using namespace std;
 
 int main() {
-    int number,copy,remainder,sum=0, fac;
-
-    cout << "Enter a natural number to test: ";
-    cin >> number;
-    copy = number;
     // Your solution code goes here
 
 
